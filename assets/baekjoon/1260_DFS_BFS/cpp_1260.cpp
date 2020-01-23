4f956bf5e962dba9003513f5757dedf76a4ab4ee
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-01-23 16:57:00 +0900

    1260

--- assets/baekjoon/1260_DFS_BFS/cpp_1260.cpp
+++ assets/baekjoon/1260_DFS_BFS/cpp_1260.cpp
@@ -5,13 +5,5 @@ using namespace std;
 
 int main() {
 	
-	int i;
-	string str;
-	cin>>i;
-	cin>>str;
-	int temp=0;
-	for(int j=0; j<i; j++){
-		temp += (str.at(j)-'0');
-	}
-	cout<<temp;
+	//DFS, BFS
 }
