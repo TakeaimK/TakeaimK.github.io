a8afdcaa6e04e986af66fec4d1b3f30de3c2d7d0
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2026-01-14 18:28:41 +0900

    fix_bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,18 +4,18 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
-import { themes as prismThemes } from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Jini\'s Blog',
-  tagline: '써먹을 수 있는 기술을 추구합니다.',
-  favicon: 'img/favicon.ico',
+  title: "Jini's Blog",
+  tagline: "써먹을 수 있는 기술을 추구합니다.",
+  favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -23,72 +23,73 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://TakeaimK.github.io',
+  url: "https://TakeaimK.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // 수식 렌더링을 위한 CSS
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'TakeaimK', // Usually your GitHub org/user name.
-  projectName: 'TakeaimK.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "TakeaimK", // Usually your GitHub org/user name.
+  projectName: "TakeaimK.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
+          routeBasePath: "/", // 블로그를 메인 페이지로 설정
           showReadingTime: true,
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -98,88 +99,88 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       colorMode: {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'Jini\'s Blog',
+        title: "Jini's Blog",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/ai-star.png',
+          alt: "My Site Logo",
+          src: "img/ai-star.png",
         },
         items: [
           // 1. AI 탭 (AI 태그 목록으로 이동)
           {
-            to: '/blog/tags/ai',
-            label: 'AI',
-            position: 'left'
+            to: "/tags/ai",
+            label: "AI",
+            position: "left",
           },
           // 2. Cloud 탭
           {
-            to: '/blog/tags/cloud',
-            label: 'Cloud',
-            position: 'left'
+            to: "/tags/cloud",
+            label: "Cloud",
+            position: "left",
           },
           // 3. Virtualization 탭
           // {
-          //   to: '/blog/tags/infra', // 태그는 소문자로 쓰는 게 안전합니다
+          //   to: '/tags/infra', // 태그는 소문자로 쓰는 게 안전합니다
           //   label: 'Infra',
           //   position: 'left'
           // },
           // 4. Daily 탭
           // {
-          //   to: '/blog/tags/daily',
+          //   to: '/tags/daily',
           //   label: 'Daily',
           //   position: 'left'
           // },
           // (선택) 전체 글 보기 탭을 맨 뒤에 두거나 뺄 수 있습니다.
           {
-            to: '/blog',
-            label: 'All Posts',
-            position: 'right'
+            to: "/",
+            label: "All Posts",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
               },
               {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
+                label: "X",
+                href: "https://x.com/docusaurus",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import  { Redirect } from '@docusaurus/router';
-
-export default function Home() {
-  // 접속하자마자 /blog 페이지로 이동시킵니다.
-  return <Redirect to="/blog" />;
-}
